c893e7ec29895d91871203faf0d5807b5550b849
Mariana Montenegro <mariana.montenegro77@gmail.com>
2026-09-17 22:56:09 -0300

    Change conftest.py

--- conftest.py
+++ conftest.py
@@ -1,19 +1,36 @@
 import pytest
 from selenium import webdriver
-
-NAVEGADOR="Chrome"
-
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+ 
+NAVEGADOR = "Chrome"
+HEADLESS = True  # ponelo en False cuando quieras VER el navegador en local
+ 
 @pytest.fixture()
 def navegador():
-    if NAVEGADOR=="Chrome":
-       driver=webdriver.Chrome()
-    elif NAVEGADOR=="Firefox": 
-         driver=webdriver.Firefox()
-    elif NAVEGADOR=="Edge":
-         driver=webdriver.Edge()
+    if NAVEGADOR == "Chrome":
+        options = ChromeOptions()
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        if HEADLESS:
+            options.add_argument("--headless=new")
+        driver = webdriver.Chrome(options=options)
+    elif NAVEGADOR == "Firefox":
+        options = FirefoxOptions()
+        if HEADLESS:
+            options.add_argument("--headless")
+        driver = webdriver.Firefox(options=options)
+    elif NAVEGADOR == "Edge":
+        options = EdgeOptions()
+        if HEADLESS:
+            options.add_argument("--headless=new")
+        driver = webdriver.Edge(options=options)
     else:
         raise Exception(f"Navegador {NAVEGADOR} no soportado")
-    driver.maximize_window()
+ 
+    driver.set_window_size(1920, 1080)
     driver.get("https://the-internet.herokuapp.com/login")
     yield driver
     driver.quit()
+ 
